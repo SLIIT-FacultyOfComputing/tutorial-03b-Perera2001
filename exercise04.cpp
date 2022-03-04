e72b7a8fe748b1ad99ad461597787574e9865588
Perera V.H.P <99681504+IT21205606@users.noreply.github.com>
2022-03-04 14:50:34 +0000

    completed

--- exercise04.cpp
+++ exercise04.cpp
@@ -16,17 +16,18 @@ int main() {
   
    input(length, width);
    print(length, width);
+  
    return 0;
 }
 
 // Do not change the print() function
 void print(int len, int wth) {
-   cout << "Length : " << len 
-        << ", Width  : " << wth << endl;
+   cout << "Length : " << len << ", Width  : " << wth << endl;
 }
 
 // Implement the Input Function here
 void input(int &length, int &width)
 {
-  
+  length = 10;
+  width = 5;
 }
